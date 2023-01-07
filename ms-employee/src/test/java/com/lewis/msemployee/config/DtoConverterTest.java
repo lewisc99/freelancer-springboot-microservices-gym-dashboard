e218f72c0498c9ec6e06d544dcfd39d0cda31181
lewisc99 <lewiscontato99@gmail.com>
2023-01-06 22:30:48 -0300

    create DtoConverterTest class part 1

---
+++ ms-employee/src/test/java/com/lewis/msemployee/config/DtoConverterTest.java
@@ -0,0 +1,16 @@
+package com.lewis.msemployee.config;
+
+
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class DtoConverterTest {
+
+
+    @BeforeEach
+    public void beforeEach()
+    {
+
+    }
+}
