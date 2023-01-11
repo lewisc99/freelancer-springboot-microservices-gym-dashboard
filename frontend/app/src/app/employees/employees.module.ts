07845799076a7db40f3cb2982cf62399b83a435b
lewisc99 <lewiscontato99@gmail.com>
2023-01-11 13:04:43 -0300

    frontend - created employee-update component

--- frontend/app/src/app/employees/employees.module.ts
+++ frontend/app/src/app/employees/employees.module.ts
@@ -3,17 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import {  RouterModule, Routes } from '@angular/router';
 import { EmployeeListComponent } from './employee-list/employee-list.component';
 import { EmployeeByIdComponent } from './employee-by-id/employee-by-id.component';
+import { EmployeeUpdateComponent } from './employee-update/employee-update.component';
 
 
 var routes:Routes = [
     {path:"employees",component:EmployeeListComponent},
-    {path: 'employees/:id', component:EmployeeByIdComponent}
+    {path: 'employees/:id', component:EmployeeByIdComponent},
+    {path: 'employees/:id/edit', component:EmployeeUpdateComponent}
 ]
 
 @NgModule({
     declarations: [
         EmployeeListComponent,
-        EmployeeByIdComponent
+        EmployeeByIdComponent,
+        EmployeeUpdateComponent
     ],
     imports: [
         BrowserModule,
