28f70a44dca66b21097acb67f6348fe53043f847
lewisc99 <lewiscontato99@gmail.com>
2023-01-11 14:59:50 -0300

    frontend - created view for employee-update component

--- frontend/app/src/app/employees/employee-update/employee-update.component.ts
+++ frontend/app/src/app/employees/employee-update/employee-update.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
 
 @Component({
   selector: 'app-employee-update',
   templateUrl: './employee-update.component.html',
   styleUrls: ['./employee-update.component.css']
 })
-export class EmployeeUpdateComponent {
+export class EmployeeUpdateComponent implements OnInit{
+
+  constructor(private route:ActivatedRoute) {}
+
+  public id:string;
+
+  ngOnInit(): void {
+
+      this.route.paramMap.subscribe( 
+        (params:any) =>
+        {
+         this.id =  params.get('id');
+        }
+      )
+  }
+
+  
 
 }
