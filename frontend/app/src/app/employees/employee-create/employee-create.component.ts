021e92b626be16cea139e6843ad66180bea545c6
lewisc99 <lewiscontato99@gmail.com>
2023-01-13 16:37:01 -0300

    frontend - created view for employee-create

--- frontend/app/src/app/employees/employee-create/employee-create.component.ts
+++ frontend/app/src/app/employees/employee-create/employee-create.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { RolesService } from '../../shared/services/roles.service';
+import { Roles } from '../domain/entities/roles';
 
 @Component({
   selector: 'app-employee-create',
   templateUrl: './employee-create.component.html',
   styleUrls: ['./employee-create.component.css']
 })
-export class EmployeeCreateComponent {
+export class EmployeeCreateComponent implements OnInit {
+
+  
+  public roles:Roles[] = [];
+  public checkBoxRoles:Map<string,boolean> = new Map<string,boolean>();
+
+  constructor(private rolesService:RolesService) {}
+
+  ngOnInit(): void {
+     this.getRoles();
+  }
+
+
+  getRoles()
+  {
+      this.rolesService.getAll().subscribe(
+        {
+          next: (result:Roles[]) =>
+          {
+               this.roles = result;
+          },
+          error: (error:any) =>
+          {
+            console.log(error);
+          }
+        }
+      )
+  }
+
+  onRolesAdded(roleName:string)
+  {
+
+  }
 
 }
