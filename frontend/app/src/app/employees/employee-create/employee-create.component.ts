db8c0d1c6ef687411c4ed8dd21799f4d07750ce9
lewisc99 <lewiscontato99@gmail.com>
2023-01-13 22:11:52 -0300

    frontend - added new method in employee-create initiateFormGroup()

--- frontend/app/src/app/employees/employee-create/employee-create.component.ts
+++ frontend/app/src/app/employees/employee-create/employee-create.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { RolesService } from '../../shared/services/roles.service';
 import { Roles } from '../domain/entities/roles';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-employee-create',
@@ -12,12 +13,31 @@ export class EmployeeCreateComponent implements OnInit {
   
   public roles:Roles[] = [];
   public checkBoxRoles:Map<string,boolean> = new Map<string,boolean>();
+  public formGroup:FormGroup;
 
-  constructor(private rolesService:RolesService) {}
+  constructor(private rolesService:RolesService, private fb:FormBuilder) {}
 
   ngOnInit(): void {
      this.getRoles();
+     this.initiateFormGroup();
   }
+
+ private initiateFormGroup(): void
+ {
+    this.formGroup = this.fb.group(
+      {
+        employeeModel: this.fb.group({
+          id: new FormControl(),
+          username: new FormControl(),
+          email: new FormControl(),
+          age: new FormControl(),
+          doc: new FormControl(),
+           roles: this.fb.array  ([])
+        })
+      }
+    )
+
+ }
 
 
   getRoles()
@@ -38,7 +58,7 @@ export class EmployeeCreateComponent implements OnInit {
 
   onRolesAdded(roleName:string)
   {
-
+   
   }
 
 }
