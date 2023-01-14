beaabf3e3338019024a8d053c5022e423d8422c5
lewisc99 <lewiscontato99@gmail.com>
2023-01-14 16:54:11 -0300

    frontend - created a new component delete-modal in shared folder

--- frontend/app/src/app/shared/shared.module.ts
+++ frontend/app/src/app/shared/shared.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from "@angular/core";
 import { BrowserModule } from '@angular/platform-browser';
 import { NavbarComponent } from "./navbar/navbar.component";
+import { DeleteModalComponent } from './delete-modal/delete-modal.component';
+import { Routes, RouterModule } from '@angular/router';
+
+const routes:Routes = [
+    {path: "employees/:id/delete", component: DeleteModalComponent}
+]
 
 
 @NgModule({
-    declarations: [NavbarComponent],
-    imports: [BrowserModule],
-    exports: [NavbarComponent]
+    declarations: [NavbarComponent, DeleteModalComponent],
+    imports: [BrowserModule, RouterModule.forChild(routes)],
+    exports: [NavbarComponent, DeleteModalComponent]
 })
 export class SharedModule {}
