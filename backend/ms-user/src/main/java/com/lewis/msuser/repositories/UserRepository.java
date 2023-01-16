636759e6176985c56029988572730b1c2fea2375
lewisc99 <lewiscontato99@gmail.com>
2023-01-16 17:25:34 -0300

    backend - ms-user - created UserRepository

---
+++ backend/ms-user/src/main/java/com/lewis/msuser/repositories/UserRepository.java
@@ -0,0 +1,10 @@
+package com.lewis.msuser.repositories;
+
+import com.lewis.msuser.entities.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.UUID;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, UUID> {
+}
