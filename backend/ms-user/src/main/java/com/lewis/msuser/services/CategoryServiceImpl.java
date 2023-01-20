dfc49d7d38b456bdb9a73610957d68d3e2cc400a
lewisc99 <lewiscontato99@gmail.com>
2023-01-20 16:22:38 -0300

    backend - ms-user - created CategoryServiceImpl and added findAll()

--- backend/ms-user/src/main/java/com/lewis/msuser/services/CategoryServiceImpl.java
+++ backend/ms-user/src/main/java/com/lewis/msuser/services/CategoryServiceImpl.java
@@ -1,11 +1,19 @@
 package com.lewis.msuser.services;
 
 import com.lewis.msuser.entities.domain.Category;
+import com.lewis.msuser.repositories.CategoryRepository;
 import com.lewis.msuser.services.contracts.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
 
 public class CategoryServiceImpl implements CategoryService {
+
+    @Autowired
+    private CategoryRepository categoryRepository;
+
     @Override
-    public Category findAll() {
-        return null;
+    public List<Category> findAll() {
+      return  categoryRepository.findAll();
     }
 }
--- backend/ms-user/src/main/java/com/lewis/msuser/services/contracts/CategoryService.java
+++ backend/ms-user/src/main/java/com/lewis/msuser/services/contracts/CategoryService.java
@@ -2,6 +2,8 @@ package com.lewis.msuser.services.contracts;
 
 import com.lewis.msuser.entities.domain.Category;
 
+import java.util.List;
+
 public interface CategoryService {
-    Category findAll();
+    List<Category> findAll();
 }
