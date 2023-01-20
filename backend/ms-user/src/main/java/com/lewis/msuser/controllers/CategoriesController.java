27b52072bb6e3011dc7da7163aa9c88bd016fa0d
lewisc99 <lewiscontato99@gmail.com>
2023-01-20 16:30:47 -0300

    backend - ms-user - categoriesController added @CrossOrigin annotation

--- backend/ms-user/src/main/java/com/lewis/msuser/controllers/CategoriesController.java
+++ backend/ms-user/src/main/java/com/lewis/msuser/controllers/CategoriesController.java
@@ -4,6 +4,7 @@ import com.lewis.msuser.entities.domain.Category;
 import com.lewis.msuser.services.contracts.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +13,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("v1/categories")
+@CrossOrigin(value = "http://localhost:4200")
 public class CategoriesController {
     @Autowired
     private CategoryService categoryService;
