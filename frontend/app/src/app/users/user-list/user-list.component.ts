4d932c031ef0be9b7267810a6e3d995dd6161fbc
lewisc99 <lewiscontato99@gmail.com>
2023-01-19 21:38:04 -0300

    frontend - ms-user - created userService

--- frontend/app/src/app/users/user-list/user-list.component.ts
+++ frontend/app/src/app/users/user-list/user-list.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { UsersDTO } from '../domain/dtos/usersDto';
+import { UserService } from '../services/user-service/user.service';
 
 @Component({
   selector: 'app-user-list',
   templateUrl: './user-list.component.html',
   styleUrls: ['./user-list.component.css']
 })
-export class UserListComponent {
+export class UserListComponent implements OnInit{
+
+
+  public usersDTO:UsersDTO = new UsersDTO();
+
+  constructor(private activatedRoute:ActivatedRoute, private userService:UserService){}
+  ngOnInit(): void {
+
+    this.activatedRoute.paramMap.subscribe(
+      params => 
+      {
+        this.getAll();
+      }
+    )
+  }
+
+  public getAll(sortBy?:string) : void
+  {
+       
+  }
 
 }
