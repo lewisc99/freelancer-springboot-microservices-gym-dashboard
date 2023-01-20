90d91d348c8f2cbc81d379a78ed581b981913605
lewisc99 <lewiscontato99@gmail.com>
2023-01-20 16:20:55 -0300

    backend - ms-user - created categoryService

---
+++ backend/ms-user/src/main/java/com/lewis/msuser/services/CategoryServiceImpl.java
@@ -0,0 +1,11 @@
+package com.lewis.msuser.services;
+
+import com.lewis.msuser.entities.domain.Category;
+import com.lewis.msuser.services.contracts.CategoryService;
+
+public class CategoryServiceImpl implements CategoryService {
+    @Override
+    public Category findAll() {
+        return null;
+    }
+}
---
+++ backend/ms-user/src/main/java/com/lewis/msuser/services/contracts/CategoryService.java
@@ -0,0 +1,7 @@
+package com.lewis.msuser.services.contracts;
+
+import com.lewis.msuser.entities.domain.Category;
+
+public interface CategoryService {
+    Category findAll();
+}
