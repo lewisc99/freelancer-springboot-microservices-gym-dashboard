7b58006697cca276ebe8fd0556d02b39e7f4898a
lewisc99 <lewiscontato99@gmail.com>
2023-01-21 11:39:24 -0300

    backend - ms-authentication - added in MyEmployeeDetailsService findByEmail method

--- backend/ms-authentication/src/main/java/com/lewis/msauthentication/services/MyEmployeeDetailsService.java
+++ backend/ms-authentication/src/main/java/com/lewis/msauthentication/services/MyEmployeeDetailsService.java
@@ -1,13 +1,37 @@
 package com.lewis.msauthentication.services;
 
+import com.lewis.msauthentication.entities.domain.Employee;
+import com.lewis.msauthentication.feignclients.EmployeeFeignClient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
 public class MyEmployeeDetailsService implements UserDetailsService {
 
+    @Autowired
+    private EmployeeFeignClient employeeFeignClient;
+
+    private static Logger logger = LoggerFactory.getLogger(UserService.class);
+
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         return null;
+    }
+
+    public Employee findByEmail(String email)
+    {
+        Employee employee = employeeFeignClient.findByEmail(email).getBody();
+
+        if(employee == null)
+        {
+            logger.error("Email not found: " + email);
+            throw new IllegalArgumentException("email not found");
+        }
+        logger.info("Email found: " + email);
+        return employee;
     }
 }
