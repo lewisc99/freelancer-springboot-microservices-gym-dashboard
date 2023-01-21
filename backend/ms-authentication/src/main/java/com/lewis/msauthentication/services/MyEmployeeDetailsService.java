efcc273f363458c7a125d00ae627c0c77b052e45
lewisc99 <lewiscontato99@gmail.com>
2023-01-21 11:02:22 -0300

    backend - ms-authentication - added feignClient interface for Employee

---
+++ backend/ms-authentication/src/main/java/com/lewis/msauthentication/services/MyEmployeeDetailsService.java
@@ -0,0 +1,13 @@
+package com.lewis.msauthentication.services;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class MyEmployeeDetailsService implements UserDetailsService {
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+    }
+}
