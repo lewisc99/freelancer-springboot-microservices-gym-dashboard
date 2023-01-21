7a79ca2c4bb2284398db497dcf43c67b280e49de
lewisc99 <lewiscontato99@gmail.com>
2023-01-21 16:20:38 -0300

    frontend - auth-module - created login component

--- frontend/app/src/app/auth/auth.module.ts
+++ frontend/app/src/app/auth/auth.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
 
 
 const routes:Routes = [
@@ -7,7 +8,9 @@ const routes:Routes = [
 ]
 
 @NgModule({
-    declarations: [],
+    declarations: [
+    LoginComponent
+  ],
     imports: [
         RouterModule.forRoot(routes)
     ],
