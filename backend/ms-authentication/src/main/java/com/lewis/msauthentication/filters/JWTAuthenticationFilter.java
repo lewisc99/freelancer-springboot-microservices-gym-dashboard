f3004107622262c93ad5f7675290eec17b3330e4
lewisc99 <lewiscontato99@gmail.com>
2023-01-21 10:56:07 -0300

    backend - ms-authentication added JWTAuthenticationFilter

---
+++ backend/ms-authentication/src/main/java/com/lewis/msauthentication/filters/JWTAuthenticationFilter.java
@@ -0,0 +1,62 @@
+package com.lewis.msauthentication.filters;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.lewis.msauthentication.entities.domain.Employee;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Date;
+
+public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+
+    private AuthenticationManager authenticationManager;
+
+    public JWTAuthenticationFilter(AuthenticationManager authenticationManager)
+    {
+        this.authenticationManager = authenticationManager;
+    }
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException
+    {
+        try
+        {
+            Employee credentials = new ObjectMapper().readValue(request.getInputStream(), Employee.class);
+
+            return authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(
+                            credentials.getUsername(),
+                            credentials.getPassword(), new ArrayList<>()
+                    )
+            );
+        }
+        catch (IOException e)
+        {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    protected  void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authResult)
+            throws IOException, ServletException
+    {
+        String token = JWT.create()
+                .withSubject(((Employee) authResult.getPrincipal()).getUsername())
+                .withExpiresAt(new Date(System.currentTimeMillis() + SecurityConstants.EXPIRATION_TIME ))
+                .sign(Algorithm.HMAC512(SecurityConstants.SECRET.getBytes()));
+
+        String body =  ((Employee) authResult.getPrincipal()).getUsername() + " " + token;
+        response.getWriter().write(body);
+        response.getWriter().flush();
+    }
+}
