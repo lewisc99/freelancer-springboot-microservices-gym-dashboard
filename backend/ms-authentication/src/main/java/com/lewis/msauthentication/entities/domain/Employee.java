99b43b8c0aa9f684c0f477d50d255a10f85c6c96
lewisc99 <lewiscontato99@gmail.com>
2023-01-21 10:19:58 -0300

    backed - ms-authentication - in Employee added UserDetails

--- backend/ms-authentication/src/main/java/com/lewis/msauthentication/entities/domain/Employee.java
+++ backend/ms-authentication/src/main/java/com/lewis/msauthentication/entities/domain/Employee.java
@@ -1,18 +1,25 @@
 package com.lewis.msauthentication.entities.domain;
 
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 import java.util.UUID;
 
-public class Employee {
+public class Employee implements UserDetails {
 
 
     private UUID id;
     private String username;
     private String email;
     private String password;
+    private boolean active;
 
     private List<Roles> roles =  new ArrayList<>();
+
     public Employee(){}
 
     public Employee(UUID id, String username, String email, String password) {
@@ -34,6 +41,7 @@ public class Employee {
         return username;
     }
 
+
     public void setUsername(String username) {
         this.username = username;
     }
@@ -46,12 +54,63 @@ public class Employee {
         this.email = email;
     }
 
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+
+
+        return checkGrantAuthorities();
+    }
+    private List<GrantedAuthority> checkGrantAuthorities() {
+
+
+        List<GrantedAuthority> authoritiesRole = new ArrayList<GrantedAuthority>();
+
+        if(username!=null && getRoles()!=null && getRoles().isEmpty()==false)
+            for(Roles roleEmployee : getRoles()){
+
+
+                authoritiesRole.add(new SimpleGrantedAuthority(roleEmployee.getName()));
+            }
+
+        return authoritiesRole;
+    }
+
     public String getPassword() {
         return password;
     }
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
     }
 
     public List<Roles> getRoles() {
