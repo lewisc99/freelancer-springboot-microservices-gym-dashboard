24728e7c1f061b58383335c492aec5d1e0d828d5
lewisc99 <lewiscontato99@gmail.com>
2023-01-21 16:18:44 -0300

    frontned - ms-auth - created new module authModule

---
+++ frontend/app/src/app/auth/auth.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from '@angular/router';
+
+
+const routes:Routes = [
+
+]
+
+@NgModule({
+    declarations: [],
+    imports: [
+        RouterModule.forRoot(routes)
+    ],
+    exports: []
+})
+export class AuthModule {}
