ac69b62df3871384d2a4772ba88deb377b641d50
lewisc99 <lewiscontato99@gmail.com>
2023-01-22 22:43:34 -0300

    backend - ms-employee - added swagger configuration

--- backend/ms-employee/src/main/java/com/lewis/msemployee/MsEmployeeApplication.java
+++ backend/ms-employee/src/main/java/com/lewis/msemployee/MsEmployeeApplication.java
@@ -2,9 +2,11 @@ package com.lewis.msemployee;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableSwagger2
 public class MsEmployeeApplication {
 
 	public static void main(String[] args) {
