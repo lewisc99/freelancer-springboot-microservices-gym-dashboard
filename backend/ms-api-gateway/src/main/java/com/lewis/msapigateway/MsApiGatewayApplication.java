2522e4c0f73149c0bf044cdb3c7e4594eea49925
lewisc99 <lewiscontato99@gmail.com>
2023-01-22 22:13:35 -0300

    backend - ms-user - added swagger, and documentation to UsersController

--- backend/ms-api-gateway/src/main/java/com/lewis/msapigateway/MsApiGatewayApplication.java
+++ backend/ms-api-gateway/src/main/java/com/lewis/msapigateway/MsApiGatewayApplication.java
@@ -1,5 +1,6 @@
 package com.lewis.msapigateway;
 
+import com.lewis.msapigateway.security.SecurityConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
--- backend/ms-api-gateway/src/main/java/com/lewis/msapigateway/security/SecurityConfiguration.java
+++ backend/ms-api-gateway/src/main/java/com/lewis/msapigateway/security/SecurityConfiguration.java
@@ -1,10 +1,14 @@
 package com.lewis.msapigateway.security;
 import com.lewis.msapigateway.services.MyEmployeeDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
@@ -13,18 +17,29 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     MyEmployeeDetailsService userDetailsService;
 
 
-
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception
-    {
-        auth.userDetailsService(userDetailsService);
-    }
-
     @Override
     public void configure(HttpSecurity http) throws Exception {
 
-        http.authorizeRequests()
-                .antMatchers( "/ms-employee/v1/employees").permitAll();
+        http.cors().and().httpBasic().and().csrf().disable().authorizeRequests();
+
+              http.authorizeRequests().antMatchers("/admin").hasRole("ADMIN")
+                .antMatchers("/user").hasAnyRole("USER","ADMIN")
+                .antMatchers("/").permitAll()
+                .antMatchers("/login").permitAll().anyRequest().authenticated().
+                and()
+                .sessionManagement();
     }
 
+    @Override
+    @Bean
+    public AuthenticationManager authenticationManagerBean() throws Exception
+    {
+        return super.authenticationManagerBean();
+    }
+
+    @Bean
+    public PasswordEncoder getpasswordEncoder()
+    {
+        return NoOpPasswordEncoder.getInstance();
+    }
 }
