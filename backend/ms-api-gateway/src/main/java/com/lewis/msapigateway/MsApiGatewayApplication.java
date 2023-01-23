8aecc6e974d44e69929efdb2284cba04d58b2819
lewisc99 <lewiscontato99@gmail.com>
2023-01-23 10:02:54 -0300

    backend - ms-api-gateway - removed all configuration from apiga-teway

--- backend/ms-api-gateway/src/main/java/com/lewis/msapigateway/MsApiGatewayApplication.java
+++ backend/ms-api-gateway/src/main/java/com/lewis/msapigateway/MsApiGatewayApplication.java
@@ -1,14 +1,11 @@
 package com.lewis.msapigateway;
 
-import com.lewis.msapigateway.security.SecurityConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableEurekaClient
-@EnableFeignClients
 public class MsApiGatewayApplication {
 
 	public static void main(String[] args) {
