f2c51f116d27bedef1016e0437ad1bb1c27d84b9
lewisc99 <lewiscontato99@gmail.com>
2023-01-25 12:59:30 -0300

    backend - removed cors from UsersController, and frontend - changed environment from users  to api-gataway

--- frontend/app/src/environments/environment.test.ts
+++ frontend/app/src/environments/environment.test.ts
@@ -2,7 +2,7 @@ export const environment = {
     production: false,
     employees_URL: "http://localhost:8765/ms-employee/v1/employees",
     roles_URL : "http://localhost:8001/v1/roles",
-    users_URL: "http://localhost:8002/v1/users",
+    users_URL: "http://localhost:8765/ms-user/v1/users",
     category_URL: "http://localhost:8002/v1/categories",
     login_URL: "http://localhost:8765/ms-auth/v1/",
   };
