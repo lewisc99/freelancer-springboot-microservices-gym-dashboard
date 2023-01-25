2aae1508411bdc763cade6533cc8575c33ae86c5
lewisc99 <lewiscontato99@gmail.com>
2023-01-25 13:03:34 -0300

    backend - removed cors from CategoryController, and frontend - changed environment from category  to api-gataway

--- frontend/app/src/environments/environment.test.ts
+++ frontend/app/src/environments/environment.test.ts
@@ -3,7 +3,7 @@ export const environment = {
     employees_URL: "http://localhost:8765/ms-employee/v1/employees",
     roles_URL : "http://localhost:8001/v1/roles",
     users_URL: "http://localhost:8765/ms-user/v1/users",
-    category_URL: "http://localhost:8002/v1/categories",
+    category_URL: "http://localhost:8765/ms-user/v1/categories",
     login_URL: "http://localhost:8765/ms-auth/v1/",
   };
   
