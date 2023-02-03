6395d52b6240d28957cfb132296ef10f8bac5f3f
lewisc99 <lewiscontato99@gmail.com>
2023-02-02 22:56:51 -0300

    created a new microservice ms-message-email

---
+++ backend/ms-message-email/src/main/java/com/lewis/msmessageemail/MsMessageEmailApplication.java
@@ -0,0 +1,13 @@
+package com.lewis.msmessageemail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsMessageEmailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsMessageEmailApplication.class, args);
+	}
+
+}
