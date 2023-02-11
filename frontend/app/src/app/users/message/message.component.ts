8b54f97ab8d4ddeb2dc3e465e05855b8e426206f
lewisc99 <lewiscontato99@gmail.com>
2023-02-11 09:48:12 -0300

    backend - ms-authentication - set token expiration to 10 min

--- frontend/app/src/app/users/message/message.component.ts
+++ frontend/app/src/app/users/message/message.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-message',
   templateUrl: './message.component.html',
   styleUrls: ['./message.component.css']
 })
-export class MessageComponent {
+export class MessageComponent implements OnInit {
 
+  public username:string;
+
+  constructor(private activatedRoute:ActivatedRoute) {}
+
+  ngOnInit(): void {
+    this.activatedRoute.queryParamMap.subscribe({
+      next: params => {
+       let username =  params.get("username")!;
+       this.username = username;
+      }
+    })
+  }
+    
 }
