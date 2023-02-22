56da68956148786c2acd8354d38503dfaad3fb4c
lewisc99 <lewiscontato99@gmail.com>
2023-02-21 21:59:05 -0300

    backend - ms-user - created MainEvent entity

---
+++ backend/ms-user/src/main/java/com/lewis/msuser/entities/models/MailEvent.java
@@ -0,0 +1,43 @@
+package com.lewis.msuser.entities.models;
+
+import java.time.LocalDate;
+
+public class MailEvent {
+
+    private String email;
+    private String subject;
+    private String text;
+    private LocalDate date;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+}
