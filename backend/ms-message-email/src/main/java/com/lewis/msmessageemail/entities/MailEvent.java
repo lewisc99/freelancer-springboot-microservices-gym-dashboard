86abf20c785a107a3acdbdb8a54d856a2551ce63
lewisc99 <lewiscontato99@gmail.com>
2023-02-23 21:16:43 -0300

    backend - ms-message-email - created listener MailListener and MailEvent entity

---
+++ backend/ms-message-email/src/main/java/com/lewis/msmessageemail/entities/MailEvent.java
@@ -0,0 +1,61 @@
+package com.lewis.msmessageemail.entities;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+
+public class MailEvent implements Serializable {
+
+    private String email;
+    private String subject;
+    private String text;
+    private LocalDate date;
+
+    public MailEvent(String email, String subject, String text, LocalDate date) {
+        this.email = email;
+        this.subject = subject;
+        this.text = text;
+        this.date = date;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    @Override
+    public String toString() {
+        return "MailEvent{" +
+                "email='" + email + '\'' +
+                ", subject='" + subject + '\'' +
+                ", text='" + text + '\'' +
+                ", date=" + date +
+                '}';
+    }
+}
---
+++ backend/ms-message-email/src/main/java/com/lewis/msmessageemail/listener/MailListener.java
@@ -0,0 +1,21 @@
+package com.lewis.msmessageemail.listener;
+
+
+import com.lewis.msmessageemail.entities.MailEvent;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.listener.adapter.ConsumerRecordMetadata;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MailListener {
+
+    @KafkaListener(topics = "user-message-topic", groupId = "group-1", containerFactory =
+    "jsonKafkaListenerContainerFactory")
+    public void mailEvent(MailEvent mailEvent, ConsumerRecordMetadata metadata)
+    {
+        System.out.println("Topic : " + metadata.topic() + " partition: ");
+        System.out.println();
+        System.out.println(mailEvent);
+
+    }
+}
