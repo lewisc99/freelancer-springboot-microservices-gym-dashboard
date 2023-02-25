69b822386f3502b7f3cca26035695bcade60ae42
lewisc99 <lewiscontato99@gmail.com>
2023-02-25 18:19:20 -0300

    backend - ms-api-gateway added swagger

--- backend/ms-api-gateway/src/main/java/com/lewis/msapigateway/MsApiGatewayApplication.java
+++ backend/ms-api-gateway/src/main/java/com/lewis/msapigateway/MsApiGatewayApplication.java
@@ -6,6 +6,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
+
 public class MsApiGatewayApplication {
 
 	public static void main(String[] args) {
--- backend/ms-api-gateway/src/main/java/com/lewis/msapigateway/config/ApiGatewayConfiguration.java
+++ backend/ms-api-gateway/src/main/java/com/lewis/msapigateway/config/ApiGatewayConfiguration.java
@@ -3,6 +3,7 @@ package com.lewis.msapigateway.config;
 import com.lewis.msapigateway.filters.AdminAuthenticationFilter;
 import com.lewis.msapigateway.filters.RolesAuthenticationFilter;
 import io.netty.resolver.DefaultAddressResolverGroup;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
@@ -11,6 +12,7 @@ import org.springframework.context.annotation.Configuration;
 import reactor.netty.http.client.HttpClient;
 
 @Configuration
+@OpenAPIDefinition
 public class ApiGatewayConfiguration {
 
     @Autowired
